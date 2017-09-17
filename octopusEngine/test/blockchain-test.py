ab4d51dacdb926e50adab16878a516920b4c24a2
Honza S. Copak <bonum.comunae@volny.cz>
2017-09-17 10:16:41 +0200

    Add files via upload

--- octopusEngine/test/blockchain-test.py
+++ octopusEngine/test/blockchain-test.py
@@ -10,22 +10,30 @@
 #https://github.com/blockchain/receive-payments-demos/blob/master/python/receive_demo/views.py
 #2017/08> https://chain.so/api/v2/get_tx_received/LTC/LNTKxPMWDNAHT1rUkAGmS81CMBHqB3W723
 #--------------------------------------------
-import urllib
-import time
-import datetime
+import time, datetime
+#from datetime import datetime
 from urllib import urlopen
-import json    
 import requests 
+import json    
+#my wallet address:
+from myWallets import  getWBTC, getWLTC
+wallAdrBTC=getWBTC()
+wallAdrLTC=getWLTC()
 
 debugPrint = True
-#my wallet address:
-wallAdrBTC="11r118H2Qv4oHfjFuJnuU8GZHGNqwEH9e"
-wallAdrLTC="LNTKxPMWDNAHT1rUkAGmS81CMBHqB3W723"
+
+#---blockchain chain.so/api
+def getLast(chainJson):
+	jsonData = chainJson.json()['data']['txs']
+	jsonDataLast = chainJson.json()['data']['txs'][len(jsonData)-1]
+	return jsonDataLast
+
 
 #---server time--- (start > RPi was off-line)
 def getServerTime():
    serverTime = urlopen("http://www.octopusengine.eu/api/datetime.php").read()
-   return str(serverTime)   
+   return str(serverTime) 
+    
    
 if (debugPrint):
    print(">>> octopusengine.org/api --- getServerTime()")	
@@ -50,32 +58,47 @@ if (debugPrint):
 
    print(">>> bitcoin last transaction ---")
 
+#------------BTC-------------------------- 
 resourceBTC = "https://chain.so/api/v2/get_tx_received/BTC/"+wallAdrBTC
 j = requests.get(resourceBTC)
 #print(j.json()['data']['txs'])  
-arrayCnt = len(j.json()['data']['txs'])
+#arrayCnt = len(j.json()['data']['txs'])
+jsonData = j.json()['data']['txs']
+jsonDataLast = j.json()['data']['txs'][len(jsonData)-1]
 
 #print(j.json()['data']['txs'][0]['time']) 
 #print(j.json()['data']['txs'][arrayCnt-1]['time'])  
-print(arrayCnt)  
-print("last >")
-print(datetime.datetime.fromtimestamp(int(j.json()['data']['txs'][arrayCnt-1]['time'])).strftime('%Y-%m-%d %H:%M:%S'))
-print(j.json()['data']['txs'][arrayCnt-1]['value'])     
+print("last > from " + str(len(jsonData)))
+print(datetime.datetime.fromtimestamp(int(jsonDataLast['time'])).strftime('%Y-%m-%d %H:%M:%S'))
+#print(j.json()['data']['txs'][arrayCnt-1]['value'])     
+print(jsonDataLast['value']) 
+time.sleep(2)
+
+#------------LTC--------------------------  
+resourceLTC = "https://chain.so/api/v2/get_tx_received/LTC/"+wallAdrLTC  
+j = requests.get(resourceLTC)
+lastTransTime = datetime.datetime.fromtimestamp(int(getLast(j)['time'])).strftime('%Y-%m-%d %H:%M:%S')
+lastTransValue = getLast(j)['value']
+print("--- litecoin last transaction ---")
+print("last > from " + str(len(jsonData)))
+print(lastTransTime)   
+print(lastTransValue) 
+
+dtTrans = datetime.datetime.strptime(lastTransTime, '%Y-%m-%d %H:%M:%S')
+dtTransUx = time.mktime(dtTrans.timetuple())
+dtServer = datetime.datetime.strptime(getServerTime(), '%Y-%m-%d %H:%M:%S')
+dtServerUx = time.mktime(dtServer.timetuple())
+dtUx =  (dtServerUx-dtTransUx) 
+
+print("")
+print("--- dateTime transaction ---")
+print(str(dtTrans) +" / "+ str(dtTransUx)) 
+print(str(dtServer) +" / "+ str(dtServerUx))
+ 
+print(str(dtServer-dtTrans) + " / "+str(dtUx)+ " /m/ "+str(int(dtUx/600))+ " /h/ "+str(int(dtUx/3600))) 
+
 time.sleep(2)
   
-print("--- litecoin last transaction ---")
-resourceLTC = "https://chain.so/api/v2/get_tx_received/LTC/"+wallAdrLTC
-#print(resourceLTC)   
-j = requests.get(resourceLTC)
-#print(j.json()['data']['txs'])  
-arrayCnt = len(j.json()['data']['txs'])
-print(arrayCnt)  
-#print(j.json()['data']['txs'][0]['time'])  
-print("last >")
-#print(j.json()['data']['txs'][arrayCnt-1]['time'])  
-print(datetime.datetime.fromtimestamp(int(j.json()['data']['txs'][arrayCnt-1]['time'])).strftime('%Y-%m-%d %H:%M:%S'))
-print(j.json()['data']['txs'][arrayCnt-1]['value'])     
-    
 #---/end 
 
  
